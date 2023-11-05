6dd635dfc8185d1a414fbb126359760672b45094
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-05 14:46:25 +0100

    Task completed

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,17 +1,26 @@
 import { TShirt } from '../components/TShirt';
 import { TShirtSetup } from '../components/TShirtSetup';
 import './style.css';
+import { useState } from 'react';
 
 export const HomePage = () => {
+  const [type, setType] = useState('');
+  const [color, setColor] = useState('');
+  const [print, setPrint] = useState('');
+
   return (
     <div className="container">
       <div className="tshirt-panel">
         <div className="tshirt-panel__tshirt">
-          <TShirt type="normalShortSleeve" color="yellow" print="Ahoj světe" />
+          <TShirt type={type} color={color} print={print} />
         </div>
         <div className="tshirt-panel__setup">
           <h2>Možnosti:</h2>
-          <TShirtSetup />
+          <TShirtSetup
+            onTypeChange={setType}
+            onColorChange={setColor}
+            onPrintChange={setPrint}
+          />
         </div>
       </div>
     </div>
